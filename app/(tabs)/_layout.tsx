d66946ff52e2dfe883efcba25bdc1edd4e978b0c
YiipHuhu <alan.gabriel@nullbug.dev>
2025-03-10 17:35:16 -0300

    icones

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'; // Exemplo de outra lib de ícones
+import { Tabs } from 'expo-router';
 
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 
 import "../../global.css"
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
+
+function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>['name']; color: string; }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
@@ -22,29 +20,33 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
       }}>
+      
+      {/* Aba 1 - Login */}
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tela 1 - login',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Login',
+          tabBarIcon: ({ color }) => <TabBarIcon name="sign-in" color={color} />,
         }}
       />
+
+      {/* Aba 2 - Home */}
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Home',
+          tabBarIcon: ({ color }) => <MaterialIcons name="home" size={28} color={color} />,
         }}
       />
+
+      {/* Aba 3 - Configurações */}
       <Tabs.Screen
         name="three"
         options={{
-          title: 'Tab Three',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Config',
+          tabBarIcon: ({ color }) => <FontAwesome name="cogs" size={28} color={color} />,
         }}
       />
     </Tabs>
